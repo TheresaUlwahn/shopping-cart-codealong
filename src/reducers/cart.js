419455d2832b75973d4d1ebb54bbf713b16f8b6c
Theresa Ulwahn <theresaulwahn@gmail.com>
2020-01-12 18:20:23 +0100

    halfway through codealong

---
+++ src/reducers/cart.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const cart = createSlice({
+    name: 'cart',
+    initialState:{
+        items: []
+    },
+    reducers: {
+        addItem: (state, action) => {
+            const existingProduct = state.items.find((item) => item.id === action.payload.id)
+            
+            if (existingProduct) {
+                existingProduct.quantity += 1
+                //increment quantity
+            } else {
+                state.items.push({ ...action.payload, quantity: 1})
+            }
+            
+        }
+    }
+})
